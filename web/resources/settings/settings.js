02f35d17e7994a52cbc68fa964cbe30b9738b2f3
Marco Realacci <info@fantasy2001.com>
2018-01-07 16:43:57 +0100

    Fixed web

---
+++ web/resources/settings/settings.js
@@ -0,0 +1,320 @@
+﻿function onStartup() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            var replaced = xhr.responseText.replace(/\\/g, '');
+            onClientsLoad(replaced);
+        }
+    }
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?get=clients&password=" + get('password'), false);
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            var replaced = xhr.responseText.replace(/\\/g, '');
+            onRoomsLoad(replaced);
+        }
+    }
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?get=rooms&password=" + get('password'), false);
+    xhr.send();
+}
+function loadRooms() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            var replaced = xhr.responseText.replace(/\\/g, '');
+            onRoomsLoad(replaced);
+        }
+    }
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?get=rooms&password=" + get('password'), false);
+    xhr.send();
+}
+function buttonaddobjectpageload()
+{
+    loadSwitchables();
+    loadButtons();
+}
+function switchbuttonaddobjectpageload() {
+    loadSwitchables();
+    loadSwitchButtons();
+}
+function loadSwitchables() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            var replaced = xhr.responseText.replace(/\\/g, '');
+            onSwitchablesLoad(replaced);
+        }
+    }
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?get=switchabledevices&password=" + get('password'), false);
+    xhr.send();
+}
+function loadButtons() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            var replaced = xhr.responseText.replace(/\\/g, '');
+            onButtonsLoad(replaced);
+        }
+    }
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?get=devices&password=" + get('password'), false);
+    xhr.send();
+}
+function loadSwitchButtons() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            var replaced = xhr.responseText.replace(/\\/g, '');
+            onSwitchButtonsLoad(replaced);
+        }
+    }
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?get=devices&password=" + get('password'), false);
+    xhr.send();
+}
+function onRoomsLoad(json) {
+    var parsed = JSON.parse(json);
+    for (index = 0; index < parsed.length; ++index) {
+
+        var x = document.getElementById("room_select");
+        var option = document.createElement("option");
+        option.text = parsed[index].Name;
+        x.add(option);
+    }
+}
+function onClientsLoad(json) {
+    var parsed = JSON.parse(json);
+    for (index = 0; index < parsed.length; ++index) {
+
+        var x = document.getElementById("client_select");
+        var option = document.createElement("option");
+        option.text = parsed[index].Name;
+        x.add(option);
+    }
+}
+function onSwitchablesLoad(json) {
+    var parsed = JSON.parse(json);
+    for (index = 0; index < parsed.length; ++index) {
+
+        var x = document.getElementById("switchables_select");
+        var option = document.createElement("option");
+        option.text = parsed[index].Name;
+        x.add(option);
+    }
+}
+function onButtonsLoad(json) {
+    var parsed = JSON.parse(json);
+    for (index = 0; index < parsed.length; ++index) {
+        if (parsed[index].ObjectType == "BUTTON") {
+            var x = document.getElementById("buttons_select");
+            var option = document.createElement("option");
+            option.text = parsed[index].Name;
+            x.add(option);
+        }
+    }
+}
+function onSwitchButtonsLoad(json) {
+    var parsed = JSON.parse(json);
+    for (index = 0; index < parsed.length; ++index) {
+        if (parsed[index].ObjectType == "SWITCH_BUTTON") {
+            var x = document.getElementById("buttons_select");
+            var option = document.createElement("option");
+            option.text = parsed[index].Name;
+            x.add(option);
+        }
+    }
+}
+function addRoom(id) {
+    var name = id.name.value;
+    var friendlynames = id.friendlynames.value;
+    var hidden = id.ifhidden.checked;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&addroom=" + name + "&setfriendlynames=" + friendlynames + "&hiddenroom=" + hidden + "&password=" + get('password'), false);
+    xhr.send();
+    alert("Request sent! Please return back");
+}
+function addLightRGB(id) {
+    var name = id.name.value;
+    var friendlynames = id.friendlynames.value;
+    var description = id.description.value;
+    var pinred = id.pinred.value;
+    var pingreen = id.pingreen.value;
+    var pinblue = id.pinblue.value;
+    var client = id.client_select.value;
+    var room = id.room_select.value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&addlightrgb=" + name + "&setfriendlynames=" + friendlynames + "&description=" + description + "&addpinr=" + pinred + "&addping=" + pingreen + "&addpinb=" + pinblue + "&client=" + client + "&room=" + room + "&password=" + get('password'), false);
+    xhr.send();
+    alert("Request sent! Please return back");
+}
+function addLightW(id) {
+    var name = id.name.value;
+    var friendlynames = id.friendlynames.value;
+    var description = id.description.value;
+    var pin = id.pin.value;
+    var client = id.client_select.value;
+    var room = id.room_select.value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&addlightw=" + name + "&setfriendlynames=" + friendlynames + "&description=" + description + "&addpin=" + pin + "&client=" + client + "&room=" + room + "&password=" + get('password'), false);
+    xhr.send();
+    alert("Request sent! Please return back");
+}
+function addRelay(id) {
+    var name = id.name.value;
+    var friendlynames = id.friendlynames.value;
+    var description = id.description.value;
+    var pin = id.pin.value;
+    var client = id.client_select.value;
+    var room = id.room_select.value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&addrelay=" + name + "&setfriendlynames=" + friendlynames + "&description=" + description + "&addpin=" + pin + "&client=" + client + "&room=" + room + "&password=" + get('password'), false);
+    xhr.send();
+    alert("Request sent! Please return back");
+}
+function addWebRelay(id) {
+    var name = id.name.value;
+    var identifier = id.identifier.value;
+    var friendlynames = id.friendlynames.value;
+    var description = id.description.value;
+    var room = id.room_select.value;
+    var createBtn = "none";
+    if (id.btnNone.checked == true) createBtn = "none";
+    if (id.btnBtn.checked == true) createBtn = "button";
+    if (id.btnSwbtn.checked == true) createBtn = "switch_button";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&addwebrelay=" + name + "&id=" + identifier + "&setfriendlynames=" + friendlynames + "&description=" + description + "&room=" + room + "&password=" + get('password') + "&createbutton=" + createBtn, false);
+    xhr.send();
+    alert(get('protocol') + '://' + get('ip') + "/api?interface=configuration&addwebrelay=" + name + "&id=" + identifier + "&setfriendlynames=" + friendlynames + "&description=" + description + "&room=" + room + "&password=" + get('password') + "&createbutton=" + createBtn);
+}
+function addSimpleFan(id) {
+    var name = id.name.value;
+    var friendlynames = id.friendlynames.value;
+    var description = id.description.value;
+    var pin = id.pin.value;
+    var client = id.client_select.value;
+    var room = id.room_select.value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&addsimplefan=" + name + "&setfriendlynames=" + friendlynames + "&description=" + description + "&addpin=" + pin + "&client=" + client + "&room=" + room + "&password=" + get('password'), false);
+    xhr.send();
+    alert("Request sent! Please return back");
+}
+function addButton(id) {
+    var name = id.name.value;
+    var pin = id.pin.value;
+    var client = id.client_select.value;
+    var room = id.room_select.value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&addbutton=" + name + "&addpin=" + pin + "&client=" + client + "&room=" + room + "&password=" + get('password'), false);
+    xhr.send();
+    alert("Request sent! Please return back");
+}
+function addBlinds(id) {
+    var name = id.name.value;
+    var friendlynames = id.friendlynames.value;
+    var description = id.description.value;
+    var room = id.room_select.value;
+    var totalsteps = id.time.value;
+
+    if (document.getElementById("btnRaspi").checked)
+    {
+        var openpin = id.openpin.value;
+        var closepin = id.closepin.value;
+        var client = id.client_select.value;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&addblinds=" + name + "&description=" + description + "&setfriendlynames=" + friendlynames + "&openpin=" + openpin + "&closepin=" + closepin + "&totalsteps=" + totalsteps + "&client=" + client + "&room=" + room + "&password=" + get('password'), false);
+        xhr.send();
+    }
+    else
+    {
+        var openpin = id.openwebrelayid.value;
+        var closepin = id.closewebrelayid.value;
+        var client = "local";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&addblinds=" + name + "&description=" + description + "&setfriendlynames=" + friendlynames + "&webrelayopenid=" + openpin + "&webrelaycloseid=" + closepin + "&totalsteps=" + totalsteps + "&client=" + client + "&room=" + room + "&password=" + get('password'), false);
+        xhr.send();
+    }
+    
+    alert("Request sent! Please return back");
+}
+function buttonAddCommand(id) {
+    var name = id.name.value;
+    var command = id.btncmd.value;
+
+    command = command.replace(/=/g, ',,').replace(/&/g, ',,,');
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&buttonaddcommand=" + name + "&command=" + command + "&password=" + get('password'), false);
+    xhr.send();
+    alert("Request sent! Please return back");
+}
+function buttonAddObject(id) {
+    var name = id.buttons_select.value;
+    var command = id.switchables_select.value;
+
+    command = command.replace(/=/g, ',,').replace(/&/g, ',,,');
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&buttonaddobject=" + name + "&object=" + command + "&password=" + get('password'), false);
+    xhr.send();
+    alert("Request sent! Please return back");
+}
+function addSwitchButton(id) {
+    var name = id.name.value;
+    var pin = id.pin.value;
+    var client = id.client_select.value;
+    var room = id.room_select.value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&addswitchbutton=" + name + "&addpin=" + pin + "&client=" + client + "&room=" + room + "&password=" + get('password'), false);
+    xhr.send();
+    alert("Request sent! Please return back");
+}
+function switchButtonAddCommand(id) {
+    var name = id.name.value;
+    var command = id.btncmd.value;
+    var pretype = id.optionon.checked;
+    var type;
+    if (pretype == true)
+    {
+        type = "on";
+    }
+    else
+    {
+        type = "off";
+    }
+
+    var xhr = new XMLHttpRequest();
+
+    command = command.replace(/=/g, ',,').replace(/&/g, ',,,');
+
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&switchbuttonaddcommand=" + name + "&command=" + command + "&type=" + type + "&password=" + get('password'), false);
+    xhr.send();
+    alert("Request sent! Please return back");
+}
+function switchButtonAddObject(id) {
+    var name = id.name.value;
+    var command = id.btncmd.value;
+
+    command = command.replace(/=/g, ',,').replace(/&/g, ',,,');
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', get('protocol') + '://' + get('ip') + "/api?interface=configuration&switchbuttonaddobject=" + name + "&object=" + command + "&password=" + get('password'), false);
+    xhr.send();
+    alert("Request sent! Please return back");
+}
+function get(name) {
+    if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+        return decodeURIComponent(name[1]);
+}
+function redirect(uri) {
+    uri = uri + document.location.search;
+    window.location.href = uri;
+}
---
+++ web/resources/switches.js
@@ -0,0 +1,242 @@
+﻿var protocol = "";
+var host = "";
+var login = "";
+var firstRun = true;
+var server;
+
+function onStartup() {
+	server = new Switchando();
+	/*var xhr = server.sendCommand("get/rooms", "/auth.html", "auth.html?data=Wrong username and / or password");
+	/*xhr.onreadystatechange = function () {
+		var data = this.responseText;
+		try {
+		} catch (ex) {
+			window.location = "/auth.html";
+			return;
+		}
+		if (data.includes("{\"status\":")) {
+			var parsed = JSON.parse(data);
+			window.location = "/auth.html?data=" + parsed.description;
+			return;
+		}
+		if (this.responseText === "") return;
+		onLoad(data);
+		componentHandler.upgradeDom();
+	};
+	xhr.send();
+	onLoad(xhr); //*/
+	var data = "";
+	try {
+		data = server.sendCommand("get/rooms", "/auth.html", "auth.html?data=Wrong username and / or password");
+	} catch (ex) {
+		window.location = "/auth.html";
+		return;
+	}
+	if (data.includes("{\"status\":")) {
+		var parsed = JSON.parse(data);
+		window.location = "/auth.html?data=" + parsed.description;
+		return;
+	}
+	onLoad(data);
+}
+function onLoad(json)
+{
+    if (firstRun == false)
+    {
+        return;
+    }
+    var parsed = JSON.parse(json);
+    var innerHtml = "";
+
+    for (index = 0; index < parsed.length; ++index) {
+
+	    innerHtml += "<br /><br />";
+	    if (parsed[index].Hidden == true) continue;
+
+	    var roomWidget = "";
+	    var mainSwitch = false;
+
+	    var objectsInnerHtml = "";
+        for (objectIndex = 0; objectIndex < parsed[index].Objects.length; ++objectIndex)
+		{
+			if (typeof parsed.status === 'undefined' && parsed[index].Objects[objectIndex].ObjectModel == null) {
+				continue;
+			}
+			var fragment = loadFile("/switchando-fragments/" + parsed[index].Objects[objectIndex].ObjectModel);
+			
+			Object.getOwnPropertyNames(parsed[index].Objects[objectIndex]).forEach(
+				function (val, idx, array) {
+					fragment = replaceAll(fragment, "%" + val + "%", parsed[index].Objects[objectIndex][val]);
+				}
+			);
+            objectsInnerHtml += fragment;
+        }
+
+        roomWidget += '<div id="div-' + parsed[index].Name + '" style="width:100%;position:relative;" class="">' +
+                    '<h4 style="color:black">' + parsed[index].Name + '</h4>' +
+                    '<div style="position:absolute;right:15px;top:0px">' +
+                    '<label style="height:100%" class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-' + parsed[index].Name + '">';
+        if (mainSwitch == false) {
+            roomWidget += '<input type="checkbox" id="switch-' + parsed[index].Name + '" class="mdl-switch__input" onClick="onClick(&quot;' + parsed[index].Name + '&quot;)" />';
+        }
+        else {
+            roomWidget += '<input type="checkbox" id="switch-' + parsed[index].Name + '" class="mdl-switch__input" onClick="onClick(&quot;' + parsed[index].Name + '&quot;)" checked />';
+        }
+        roomWidget += '<span class="mdl-switch__label"></span>' +
+            '</label>' +
+            '</div>' +
+            '</div>';
+
+        innerHtml += roomWidget;
+        innerHtml += objectsInnerHtml;
+
+    }
+    document.getElementById('switches_div').innerHTML += innerHtml;
+    collapse();
+	firstRun = false;
+	setInterval(async function () {
+		updateSwitches();
+    }, 3000);
+
+    $('#search').on('input', function () {
+        var pageDivs = document.getElementsByClassName("switchdiv");
+        if ($(this).val() == "")
+        {
+            for (i = 0; i < pageDivs.length; i++) {
+                 pageDivs[i].style.display = "";
+            }
+            return;
+        }
+        for (i = 0; i < pageDivs.length; i++) {
+            var id = pageDivs[i].id.substring(4).toLowerCase();
+            if (id.includes($(this).val())) {
+                pageDivs[i].style.display = "";
+            } else pageDivs[i].style.display = "none";
+        }
+	});
+	setTimeout(async function () {
+		loadSwitches(json);
+	}, 250);
+}
+function updateSwitches() {
+	var xhr = server.buildRequest("get/rooms");
+	xhr.onreadystatechange = function () {
+		if (this.responseText === "") return;
+		loadSwitches(this.responseText);
+	};
+	xhr.send();
+}
+async function loadSwitches(json) {
+    var pageDivs = await document.getElementsByClassName("mdl-switch__input");
+    var parsed = await JSON.parse(json);
+
+    for (index = 0; index < parsed.length; ++index) {
+        if (parsed[index].Hidden == true) continue;
+        var mainSwitch = false;
+        for (objectIndex = 0; objectIndex < parsed[index].Objects.length; ++objectIndex) {
+            if (parsed[index].Objects[objectIndex].Switch != undefined) {
+                if (parsed[index].Objects[objectIndex].Switch == true) {
+                    var myCheckbox = document.getElementById('switch-' + parsed[index].Objects[objectIndex].Name);
+                    myCheckbox.parentElement.MaterialSwitch.on();
+                    mainSwitch = true;
+                }
+                else
+                {
+                    var myCheckbox = document.getElementById('switch-' + parsed[index].Objects[objectIndex].Name);
+                    myCheckbox.parentElement.MaterialSwitch.off();
+                }
+            }
+        }
+        if (mainSwitch) {
+            var myCheckbox = document.getElementById('switch-' + parsed[index].Name);
+            myCheckbox.parentElement.MaterialSwitch.on();
+        }
+        else {
+            var myCheckbox = document.getElementById('switch-' + parsed[index].Name);
+            myCheckbox.parentElement.MaterialSwitch.off();
+        }
+    }
+}
+
+function onClick(target)
+{
+	var xhr;
+    if (document.getElementById('switch-' + target).checked)
+    {
+		xhr = server.buildRequest("id/" + target + "/switch?objname=" + target + "&switch=true");
+    }
+	else xhr = server.buildRequest("id/" + target + "/switch?objname=" + target + "&switch=false");
+
+	xhr.onreadystatechange = function () {
+		updateSwitches();
+	};
+	xhr.send();
+}
+function switchOn(target) {
+	var xhr = server.buildRequest("id/" + target + "/switch?objname=" + target + "&switch=true");
+	xhr.onreadystatechange = function () {
+		updateSwitches();
+	};
+	xhr.send();
+}
+function switchOff(target) {
+	var xhr = server.buildRequest("id/" + target + "/switch?objname=" + target + "&switch=false");
+	xhr.onreadystatechange = function () {
+		updateSwitches();
+	};
+	xhr.send();
+}
+
+function get(name) {
+    if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+        return decodeURIComponent(name[1]);
+}
+function show() {
+document.getElementById('fsbtn').style.display = 'none';
+	var elem = document.body;
+        if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
+            if (elem.requestFullScreen) {
+                elem.requestFullScreen();
+            } else if (elem.mozRequestFullScreen) {
+                elem.mozRequestFullScreen();
+            } else if (elem.webkitRequestFullScreen) {
+                elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+            } else if (elem.msRequestFullscreen) {
+                elem.msRequestFullscreen();
+            }
+        } else {
+            if (document.cancelFullScreen) {
+                document.cancelFullScreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitCancelFullScreen) {
+                document.webkitCancelFullScreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            }
+        }
+}
+function collapse() {
+    $(".option-content").hide();
+    $(".arrow-up").hide();
+    $(".option-heading").click(function () {
+        $(this).next(".option-content").slideToggle(500);
+        $(this).find(".arrow-up, .arrow-down").toggle(500);
+    });
+}
+function loadFile(filePath) {
+    var result = "";
+    var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", filePath, false);
+	try {
+		xmlhttp.send();
+		if (xmlhttp.status == 200) {
+			result = xmlhttp.responseText;
+		}
+		return result;
+	}
+	catch(e) { return ""; }
+}
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+}
